1078c7fc200968ed4e0a79f89368847ea8d0cdc8
Keen <austin.kuehne@gmail.com>
2018-07-16 13:18:24 -0700

    done

---
+++ Hard-Mode/main.js
@@ -0,0 +1,34 @@
+function showTime() {
+	var date = new Date()
+	var h = date.getHours()
+	var m = date.getMinutes()
+	var s = date.getSeconds()
+
+	if(h == 0) {
+		h = 12
+	}
+
+	if (h > 12) {
+		h = h - 12
+	}
+
+	if (h < 10) {
+		h = "0" + h
+	}
+
+	if (m < 10) {
+		m = "0" + m
+	}
+
+	if (s < 10) {
+		s = "0" + s
+	}
+
+	var color = "#" + h + m + s
+
+	document.getElementById("clock").innertext = color
+	document.getElementById("clock").textContent = color
+	document.body.style.background = color
+}
+
+setInterval(showTime, 10)
---
+++ main.js
@@ -0,0 +1,29 @@
+function showTime() {
+	var date = new Date()
+	var h = date.getHours()
+	var m = date.getMinutes()
+	var s = date.getSeconds()
+
+	if(h == 0) {
+		h = 12
+	}
+
+	if (h > 12) {
+		h = h - 12
+	}
+	
+	if (m < 10) {
+		m = "0" + m
+	}
+
+	if (s < 10) {
+		s = "0" + s
+	}
+
+	var time = h + ":" + m + ":" + s
+
+	document.getElementById("clock").innertext = time
+	document.getElementById("clock").textContent = time
+}
+
+setInterval(showTime, 1000)
